6afc0263b4590179b1df5bd74fb0a23ba53f36ed
Timothy Morey <tmorey@esri.com>
2023-01-15 10:14:55 -0500

    more controls, fewer constants

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const STYLE_STORAGE_KEY = 'staff-pages-line-style'
+const STORAGE_KEY_BASE = 'staff-pages'
 
-function getCanvas() {
-  return document.querySelector('#the-canvas')
-}
-
-function render(canvas, lineStyle) {
+function render() {
+  const canvas = canvasElement()
   canvas.width = canvas.clientWidth * window.devicePixelRatio
   canvas.height = canvas.clientHeight * window.devicePixelRatio
   const context = canvas.getContext('2d')
@@ -15,22 +12,22 @@ function render(canvas, lineStyle) {
   context.strokeStyle = 'black'
 
   let y = 1
-  if (lineStyle === 'staff') {
+  if (lineStyle() === 'staff') {
     for (let groups = 0; groups < 9; groups++) {
-      y = drawStaff(context, y, 10)
-      y += 62
+      y = drawStaff(context, y, lineSpacing())
+      y += groupSpacing()
     }
-  } else if (lineStyle === 'tab') {
+  } else if (lineStyle() === 'tab') {
     for (let groups = 0; groups < 8; groups++) {
-      y = drawTabLines(context, y, 10)
-      y += 64
+      y = drawTabLines(context, y, lineSpacing())
+      y += groupSpacing()
     }
-  } else if (lineStyle === 'staff-and-tab') {
+  } else if (lineStyle() === 'staff-and-tab') {
     for (let groups = 0; groups < 5; groups++) {
-      y = drawStaff(context, y, 10)
-      y += 34
-      y = drawTabLines(context, y, 10)
-      y += 60
+      y = drawStaff(context, y, lineSpacing())
+      y += staffSpacing()
+      y = drawTabLines(context, y, lineSpacing())
+      y += groupSpacing()
     }
   }
 }
@@ -61,33 +58,91 @@ function drawLine(context, y) {
   return y
 }
 
-function getSelectElement() {
+function canvasElement() {
+  return document.querySelector('#the-canvas')
+}
+
+function selectElement() {
   return document.querySelector('#style-select')
 }
 
-function getSelectedLineStyle() {
-  return getSelectElement().value
+function lineStyle() {
+  return selectElement().value
 }
 
-function hydrateFromLocalStorage() {
-  if (window.localStorage.getItem(STYLE_STORAGE_KEY)) {
-    getSelectElement().value = window.localStorage.getItem(STYLE_STORAGE_KEY)
-  }
+function setLineStyle(lineStyle) {
+  selectElement().value = lineStyle
 }
 
-function saveToLocalStorage(lineStyle) {
-  window.localStorage.setItem(STYLE_STORAGE_KEY, lineStyle)
+function lineSpacingInputElement() {
+  return document.querySelector('#line-spacing-input')
+}
+
+function lineSpacing() {
+  return parseInt(lineSpacingInputElement().value)
+}
+
+function setLineSpacing(spacing) {
+  lineSpacingInputElement().value = spacing
+}
+
+function staffSpacingInputElement() {
+  return document.querySelector('#staff-spacing-input')
+}
+
+function staffSpacing() {
+  return parseInt(staffSpacingInputElement().value)
+}
+
+function setStaffSpacing(spacing) {
+  staffSpacingInputElement().value = spacing
+}
+
+function groupSpacingInputElement() {
+  return document.querySelector('#group-spacing-input')
+}
+
+function groupSpacing() {
+  return parseInt(groupSpacingInputElement().value)
+}
+
+function setGroupSpacing(spacing) {
+  groupSpacingInputElement().value = spacing
+}
+
+function hydrateControls() {
+  setLineStyle(window.localStorage.getItem(`${STORAGE_KEY_BASE}-line-style`) ?? 'staff-and-tab')
+  setLineSpacing(window.localStorage.getItem(`${STORAGE_KEY_BASE}-${lineStyle()}-line-spacing`) ?? 10)
+  setStaffSpacing(window.localStorage.getItem(`${STORAGE_KEY_BASE}-${lineStyle()}-staff-spacing`) ?? 34)
+  setGroupSpacing(window.localStorage.getItem(`${STORAGE_KEY_BASE}-${lineStyle()}-group-spacing`) ?? 60)
+}
+
+function saveToLocalStorage() {
+  window.localStorage.setItem(`${STORAGE_KEY_BASE}-line-style`, lineStyle())
+  window.localStorage.setItem(`${STORAGE_KEY_BASE}-${lineStyle()}-line-spacing`, lineSpacing())
+  window.localStorage.setItem(`${STORAGE_KEY_BASE}-${lineStyle()}-staff-spacing`, staffSpacing())
+  window.localStorage.setItem(`${STORAGE_KEY_BASE}-${lineStyle()}-group-spacing`, groupSpacing())
+}
+
+function handleInput() {
+  saveToLocalStorage()
+  render()
+}
+
+function handleStyleChange() {
+  setLineSpacing(window.localStorage.getItem(`${STORAGE_KEY_BASE}-${lineStyle()}-line-spacing`) ?? 10)
+  setStaffSpacing(window.localStorage.getItem(`${STORAGE_KEY_BASE}-${lineStyle()}-staff-spacing`) ?? 34)
+  setGroupSpacing(window.localStorage.getItem(`${STORAGE_KEY_BASE}-${lineStyle()}-group-spacing`) ?? 60)
+  saveToLocalStorage()
+  render()
 }
 
 window.addEventListener('load', () => {
-  hydrateFromLocalStorage()
-  let lineStyle = getSelectedLineStyle()
-  const canvasElement = getCanvas()
-  render(canvasElement, lineStyle)
-  window.addEventListener('resize', () => render(canvasElement, lineStyle))
-  getSelectElement().addEventListener('input', () => {
-    lineStyle = getSelectedLineStyle()
-    saveToLocalStorage(lineStyle)
-    render(canvasElement, lineStyle)
-  })
+  hydrateControls()
+  render()
+  window.addEventListener('resize', render)
+  selectElement().addEventListener('input', handleStyleChange)
+  lineSpacingInputElement().addEventListener('input', handleInput)
+  staffSpacingInputElement().addEventListener('input', handleInput)
+  groupSpacingInputElement().addEventListener('input', handleInput)
 })
